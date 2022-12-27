3f78c426aa28ff6aa6b7367cf592e00c6d4d4829
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2022-12-26 18:56:51 -0600

    Create Dog model

---
+++ models/dogModel.js
@@ -0,0 +1,25 @@
+
+const mongoose = require("mongoose");
+
+const dogSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please enter a name"]
+    },
+    avatar: {
+        type: Object,
+        default: {},
+    },
+    age: {
+        type: Number,
+        required: [true, "Please add an age"]
+    },
+    bio: {
+        type: String,
+        required: [true, "Please enter description"]
+    },
+})
+
+const Dog = mongoose.model("Dog", dogSchema);
+
+module.exports = Dog
